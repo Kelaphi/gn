a24e9ccacbee9da9958bc4ba77e0a8aed97dd4b8
Kelaphi <140216050+Kelaphi@users.noreply.github.com>
2025-05-10 16:38:34 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const config = {
     serverInfo: {
         serverLogoImageFileName: "logo.png", /*This is a file name for logo in /images/ (If you upload new logo with other name, you must change this value)*/
         serverName: "Golden Network", /*Server name*/
-        serverIp: "gz.feathermc.gg", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
+        serverIp: "goldnn.xyz", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
         discordServerID: "1279266584195760251" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
     },
 
